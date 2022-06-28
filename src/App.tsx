9fc5a5672252dc101b24c62af3e2efd82f1ad9fc
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-28 12:55:12 +0200

    Adding map first test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { Icon } from "leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "../src/App.css";
 
 const App = () => {
-  return <div>In Progress</div>;
+  return (
+    <MapContainer
+      center={[41.390205, 2.154007]}
+      zoom={12}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
 };
 
 export default App;
