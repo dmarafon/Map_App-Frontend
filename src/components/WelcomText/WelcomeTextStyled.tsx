64bba97812c4db71829b5ffb42d586f63e9d25c4
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-04 12:44:21 +0200

    Abstracting to another component the Welcome Text

--- src/components/WelcomText/WelcomeTextStyled.tsx
+++ src/components/WelcomText/WelcomeTextStyled.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const WelcomePageStyled = styled.div`
+const WelcomeTextStyled = styled.div`
   background: rgb(232, 29, 253);
   background: linear-gradient(176deg, #fd1d89 4%, rgba(252, 124, 69, 1) 100%);
 
@@ -36,4 +36,4 @@ const WelcomePageStyled = styled.div`
   }
 `;
 
-export default WelcomePageStyled;
+export default WelcomeTextStyled;
--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,26 +1,12 @@
 import RegisterLoginForm from "../components/RegisterLoginForm/RegisterLoginForm";
-import WelcomePageStyled from "./WelcomePageStyled";
+import WelcomeText from "../components/WelcomText/WelcomeText";
 
 const WelcomePage = (): JSX.Element => {
   return (
-    <WelcomePageStyled>
-      <div className="welcome__text--container">
-        <img
-          className="welcome__image--logo"
-          src="/images/trippy_logo314.webp"
-          alt="trippy logo"
-          height={150}
-        />{" "}
-        <p className="welcome__paragraph--regular_text_first">
-          The most Lightweight service
-        </p>
-        <p className="welcome__paragraph--regular_text_second">
-          to Store your favorite places
-        </p>
-        <p className="welcome__paragraph--regular_text_third">in the Globe!</p>
-      </div>
+    <>
+      <WelcomeText />
       <RegisterLoginForm />
-    </WelcomePageStyled>
+    </>
   );
 };
 
