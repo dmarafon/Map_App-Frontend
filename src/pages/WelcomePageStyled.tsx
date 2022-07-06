214377d4001e439a1c1bc21f7f57f22532fd46e6
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-06 17:24:01 +0200

    Adding correction for responsive CSS

--- src/pages/WelcomePageStyled.tsx
+++ src/pages/WelcomePageStyled.tsx
@@ -3,46 +3,50 @@ import styled from "styled-components";
 const WelcomePageStyled = styled.div`
   .welcome__container {
     display: flex;
-    column-gap: 35vw;
+    column-gap: 38vw;
     align-items: flex-start;
 
-    @media (min-width: 1380px) and (max-width: 1474px) {
-      column-gap: 32vw;
-    }
-    @media (min-width: 1330px) and (max-width: 1379px) {
+    @media (min-width: 1475px) and (max-width: 1580px) {
       column-gap: 30vw;
     }
 
+    @media (min-width: 1380px) and (max-width: 1474px) {
+      column-gap: 23vw;
+    }
+    @media (min-width: 1330px) and (max-width: 1379px) {
+      column-gap: 21vw;
+    }
+
     @media (min-width: 1300px) and (max-width: 1329px) {
-      column-gap: 28vw;
+      column-gap: 19vw;
     }
 
     @media (min-width: 1250px) and (max-width: 1299px) {
-      column-gap: 25vw;
+      column-gap: 17vw;
     }
 
     @media (min-width: 1220px) and (max-width: 1249px) {
-      column-gap: 22vw;
-    }
-
-    @media (min-width: 1190px) and (max-width: 1219px) {
-      column-gap: 20vw;
-    }
-
-    @media (min-width: 1150px) and (max-width: 1189px) {
-      column-gap: 18vw;
-    }
-
-    @media (min-width: 1100px) and (max-width: 1149px) {
       column-gap: 15vw;
     }
 
+    @media (min-width: 1190px) and (max-width: 1219px) {
+      column-gap: 11vw;
+    }
+
+    @media (min-width: 1150px) and (max-width: 1189px) {
+      column-gap: 9vw;
+    }
+
+    @media (min-width: 1100px) and (max-width: 1149px) {
+      column-gap: 7vw;
+    }
+
     @media (min-width: 1070px) and (max-width: 1099px) {
-      column-gap: 12vw;
+      column-gap: 6vw;
     }
 
     @media (min-width: 1000px) and (max-width: 1069px) {
-      column-gap: 10vw;
+      column-gap: 5vw;
     }
 
     @media (max-width: 1022px) {
