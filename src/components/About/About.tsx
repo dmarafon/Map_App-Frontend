22e87664ba991bde9d2edb1ce7a0d4e14917d6b2
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-08 01:59:57 +0200

    Further building new CSS features for about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -26,11 +26,29 @@ const About = (): JSX.Element => {
       <AboutStyled>
         <div className="about__container">
           {!elementVisible && (
-            <img
-              className="about__image--globe"
-              src="https://firebasestorage.googleapis.com/v0/b/trippy-dc13b.appspot.com/o/ezgif.com-gif-maker%20(1).webp?alt=media&token=b91245b1-1e8a-4f37-9ad9-43c3428566d4"
-              alt="illustration of a world spinning with markers"
-            />
+            <div className="about__info--container">
+              <div>
+                <p className="about__paragraph--text">
+                  {" "}
+                  &gt; Save Your favorite Places in the Globe
+                </p>
+                <p className="about__paragraph--text">
+                  {" "}
+                  &gt; Filter by your own created Categories
+                </p>
+                <p className="about__paragraph--text">
+                  {" "}
+                  &gt; Save Your favorite Places in the Globe
+                </p>
+              </div>
+              <div>
+                <img
+                  className="about__image--globe"
+                  src="https://firebasestorage.googleapis.com/v0/b/trippy-dc13b.appspot.com/o/ezgif.com-gif-maker%20(1).webp?alt=media&token=b91245b1-1e8a-4f37-9ad9-43c3428566d4"
+                  alt="illustration of a world spinning with markers"
+                />
+              </div>
+            </div>
           )}
         </div>
       </AboutStyled>
--- src/components/About/AboutStyled.tsx
+++ src/components/About/AboutStyled.tsx
@@ -3,17 +3,68 @@ import styled from "styled-components";
 const AboutStyled = styled.div`
   .about__container {
     height: 600px;
+    margin-left: 35px;
+  }
+
+  .about__info--container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    gap: 10px;
+  }
+
+  .about__paragraph--text {
+    font-family: "Cairo", sans-serif;
+    font-size: 30px;
+    font-weight: 700;
+    animation: fadein 2s;
+    -moz-animation: fadein 2s;
+    -webkit-animation: fadein 2s;
+    -o-animation: fadein 2s;
+  }
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @-moz-keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @-webkit-keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @-o-keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 
   .about__image--globe {
     display: inline-block;
-    animation: move-up 2s forwards;
+    animation: move-up 1.5s forwards;
   }
 
   @keyframes move-up {
     0% {
       opacity: 0;
-      transform: translateY(5vh);
+      transform: translateY(20vh);
     }
     100% {
       opacity: 1;
